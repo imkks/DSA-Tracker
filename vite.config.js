945981f389915e6af0150a998dc4bf59523dc927
Krishna <imkrsh007@gmail.com>
2026-01-04 00:52:25 +0530

    github deployement

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  // REPLACE 'dsa-tracker' WITH YOUR EXACT REPO NAME
+  base: '/DSA-Tracker/', 
 })
